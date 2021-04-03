8f1c9bae46f89e92beaf364ef6635acebe46e4de
Ryan Wirth <ryan@ryanwirth.ca>
2021-04-03 17:19:44 -0600

    Create Navigation component

---
+++ src/common/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <div>Logo</div>
+    
+    <ul>
+      <li>
+        <router-link to="/dashboard">Dashboard</router-link>
+      </li>
+      <li>
+        <router-link to="/accounts">Accounts</router-link>
+      </li>
+      <li>
+        <router-link to="/positions">Positions</router-link>
+      </li>
+      <li>
+        <router-link to="/strategies">Strategies</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
--- src/modules/dashboard/Dashboard.vue
+++ src/modules/dashboard/Dashboard.vue
@@ -1,3 +1,15 @@
 <template>
+  <Navigation />
   <div>Dashboard</div>
 </template>
+
+<script>
+import Navigation from '@/common/Navigation.vue'
+
+export default {
+  name: 'Dashboard',
+  components: {
+    Navigation
+  }
+}
+</script>
