f00c9d662cddfc03e91a6f3a2c22bf2efcea11f8
Ryan Wirth <ryan@ryanwirth.ca>
2021-04-03 16:50:10 -0600

    Add Firebase service

---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp, analytics, firestore } from 'firebase'
+
+export const firebase = initializeApp({
+  apiKey: "AIzaSyABdBUfwLvQgk0JkuqI7_smbfVQnukxlcM",
+  authDomain: "stacks-5e258.firebaseapp.com",
+  projectId: "stacks-5e258",
+  storageBucket: "stacks-5e258.appspot.com",
+  messagingSenderId: "674462185107",
+  appId: "1:674462185107:web:8f86ad402eaae82232ff66",
+  measurementId: "G-X5LLTM257W"
+})
+
+export const analytics = analytics()
+export const firestore = firestore()
