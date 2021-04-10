1da9c8773f00808b85662aa501c2e5175efd2322
Ryan Wirth <ryan@ryanwirth.ca>
2021-04-10 15:32:17 -0600

    Pass in metric props

--- src/modules/dashboard/Dashboard.vue
+++ src/modules/dashboard/Dashboard.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="dashboard">
     <div class="dashboard__metrics">
-      <metric />
-      <metric />
-      <metric />
+      <metric label="All Over Value" :value="2341558.76" />
+      <metric label="Return" :value="1217610.16" :change="0.521" />
+      <metric label="Last 1 Year" :value="140493.86" :change="0.0602" />
     </div>
     <growth-chart />
   </div>
--- src/modules/dashboard/components/Metric.vue
+++ src/modules/dashboard/components/Metric.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="metric">
-    Metric
+    <div class="metric__label">{{ label }}</div>
+    <div class="metric__value">{{ formattedValue }}</div>
+    <div v-if="change" class="metric__change">{{ formattedChange }}</div>
   </div>
 </template>
+
+<script>
+import numeral from "numeral";
+
+export default {
+  props: {
+    label: String,
+    value: Number,
+    change: Number,
+  },
+  computed: {
+    formattedValue() {
+      return numeral(this.value).format("$0,0.00");
+    },
+    formattedChange() {
+      return numeral(this.change).format("0.00%");
+    }
+  }
+};
+</script>
