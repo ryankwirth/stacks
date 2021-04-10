ac1d41e91edf8cec0417c08266e53ca44f3a49d7
Ryan Wirth <ryan@ryanwirth.ca>
2021-04-10 15:11:18 -0600

    Create separate LineChart component

--- src/modules/dashboard/Dashboard.vue
+++ src/modules/dashboard/Dashboard.vue
@@ -1,43 +1,15 @@
 <template>
   <div class="dashboard">
-    <growth-chart :chart-data="chartData" />
+    <growth-chart />
   </div>
 </template>
 
 <script>
 import GrowthChart from "./components/GrowthChart.vue";
 
-const days = [...Array(365).keys()];
-const labels = days.map((value) => new Date(1585769228000 + 86400000 * value));
-const lineData = days.map((value) => Math.random() * value * 5_000);
-const barData = days.map((value) => Math.random() * (value / 365) * 0.025);
-
 export default {
   components: {
     GrowthChart,
-  },
-  computed: {
-    chartData() {
-      return {
-        labels,
-        datasets: [
-          {
-            type: "line",
-            yAxisID: "dollars",
-            data: lineData,
-            pointRadius: 0,
-            borderWidth: 1.5,
-            borderColor: "#00dd6a",
-          },
-          {
-            type: "bar",
-            yAxisID: "return",
-            data: barData,
-            backgroundColor: "#3e4345",
-          },
-        ],
-      };
-    },
   },
 };
 </script>
--- src/modules/dashboard/components/GrowthChart.vue
+++ src/modules/dashboard/components/GrowthChart.vue
@@ -1,14 +1,45 @@
+<template>
+  <line-chart :chart-data="chartData" :options="options" />
+</template>
+
 <script>
-import { Line, mixins } from "vue-chartjs";
+import LineChart from "@/common/components/LineChart.vue";
 import { createOptions } from "./GrowthChart.config";
 
-const { reactiveProp } = mixins;
+const days = [...Array(365).keys()];
+const labels = days.map((value) => new Date(1585769228000 + 86400000 * value));
+const lineData = days.map((value) => Math.random() * value * 5_000);
+const barData = days.map((value) => Math.random() * (value / 365) * 0.025);
 
 export default {
-  extends: Line,
-  mixins: [reactiveProp],
-  mounted() {
-    this.renderChart(this.chartData, createOptions());
+  components: {
+    LineChart,
+  },
+  computed: {
+    chartData() {
+      return {
+        labels,
+        datasets: [
+          {
+            type: "line",
+            yAxisID: "dollars",
+            data: lineData,
+            pointRadius: 0,
+            borderWidth: 1.5,
+            borderColor: "#00dd6a",
+          },
+          {
+            type: "bar",
+            yAxisID: "return",
+            data: barData,
+            backgroundColor: "#3e4345",
+          },
+        ],
+      };
+    },
+    options() {
+      return createOptions();
+    },
   },
 };
 </script>
