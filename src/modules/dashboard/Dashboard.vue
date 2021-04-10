d3f0be8a3f2c68d8fe7521ce11a34e3101982c23
Ryan Wirth <ryan@ryanwirth.ca>
2021-04-10 15:23:10 -0600

    Create Metric component

--- src/modules/dashboard/Dashboard.vue
+++ src/modules/dashboard/Dashboard.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="dashboard">
+    <div class="dashboard__metrics">
+      <metric />
+      <metric />
+      <metric />
+    </div>
     <growth-chart />
   </div>
 </template>
 
 <script>
 import GrowthChart from "./components/GrowthChart.vue";
+import Metric from "./components/Metric.vue";
 
 export default {
   components: {
     GrowthChart,
+    Metric,
   },
 };
 </script>
+
+<style>
+.dashboard__metrics {
+  display: flex;
+}
+</style>
---
+++ src/modules/dashboard/components/Metric.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="metric">
+    Metric
+  </div>
+</template>
