4a51e960d6870234940c27fae7f0a3b8f927210b
Ryan Wirth <ryan@ryanwirth.ca>
2021-04-10 11:24:30 -0600

    Create GrowthChart

--- src/modules/dashboard/Dashboard.vue
+++ src/modules/dashboard/Dashboard.vue
@@ -1,3 +1,38 @@
 <template>
   <div>Dashboard</div>
+  <growth-chart :chart-data="chartData"></growth-chart>
 </template>
+
+<script>
+import GrowthChart from "./components/GrowthChart.vue";
+
+export default {
+  components: {
+    GrowthChart,
+  },
+  computed: {
+    chartData() {
+      return {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [this.getRandomInt(), this.getRandomInt()],
+          },
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [this.getRandomInt(), this.getRandomInt()],
+          },
+        ],
+      };
+    },
+  },
+  methods: {
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    },
+  },
+};
+</script>
