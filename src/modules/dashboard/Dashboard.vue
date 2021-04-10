9faeb3bb8731433859aaacdca0cd5ff15fae4982
Ryan Wirth <ryan@ryanwirth.ca>
2021-04-10 17:46:00 -0600

    Create RangeSelector component

--- src/modules/dashboard/Dashboard.vue
+++ src/modules/dashboard/Dashboard.vue
@@ -2,6 +2,7 @@
   <div class="dashboard">
     <div class="dashboard__header">
       <metrics />
+      <range-selector />
     </div>
     <growth-chart />
   </div>
@@ -10,11 +11,13 @@
 <script>
 import GrowthChart from "./components/GrowthChart.vue";
 import Metrics from "./components/Metrics.vue";
+import RangeSelector from "./components/RangeSelector.vue";
 
 export default {
   components: {
     GrowthChart,
     Metrics,
+    RangeSelector,
   },
 };
 </script>
@@ -22,5 +25,7 @@ export default {
 <style>
 .dashboard__header {
   display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 </style>
---
+++ src/modules/dashboard/components/RangeSelector.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="range-selector">
+    <div class="range-selector__option">1W</div>
+    <div class="range-selector__option">1M</div>
+    <div class="range-selector__option">3M</div>
+    <div class="range-selector__option">YTD</div>
+    <div class="range-selector__option range-selector__option--selected">
+      1Y
+    </div>
+    <div class="range-selector__option">3Y</div>
+    <div class="range-selector__option">All</div>
+  </div>
+</template>
+
+<style lang="scss">
+.range-selector {
+  display: flex;
+}
+
+.range-selector__option {
+  margin: 0 4px;
+  padding: 8px 12px;
+
+  font-size: 15px;
+  cursor: pointer;
+
+  &:hover {
+    font-weight: bold;
+  }
+}
+
+.range-selector__option--selected {
+  font-weight: bold;
+  border-radius: 4px;
+  background: $color-neutral-700;
+}
+</style>
