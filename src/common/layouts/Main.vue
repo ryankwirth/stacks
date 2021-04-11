30ef3dfd0cfea8e8d9a152984066aabae2cc5659
Ryan Wirth <ryan@ryanwirth.ca>
2021-04-10 21:41:59 -0600

    Wrap main components to apply separate background colors

--- src/common/layouts/Main.vue
+++ src/common/layouts/Main.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="main">
-    <Navigation />
-    <router-view />
+    <div class="main__navigation">
+      <div class="main__wrapper">
+        <Navigation />
+      </div>
+    </div>
+    <div class="main__contents">
+      <div class="main__wrapper">
+        <router-view />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,8 +23,24 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 .main {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.main__navigation {
+  background: $color-neutral-900;
+}
+
+.main__contents {
+  flex: 1;
+  padding: 32px 0;
+  background: $color-neutral-800;
+}
+
+.main__wrapper {
   margin: 0 auto;
   max-width: 1366px;
 }
