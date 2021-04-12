3ff78d91da0962a062c3dbc773fa1ad57e642779
Ryan Wirth <ryan@ryanwirth.ca>
2021-04-11 18:12:34 -0600

    Create Account component

--- src/modules/dashboard/cards/accounts/Accounts.vue
+++ src/modules/dashboard/cards/accounts/Accounts.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="accounts">
     Your Accounts
+    <account />
   </div>
 </template>
+
+<script>
+import Account from "./components/Account.vue";
+
+export default {
+  components: {
+    Account,
+  },
+};
+</script>
---
+++ src/modules/dashboard/cards/accounts/components/Account.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="account">
+    Account!
+  </div>
+</template>
