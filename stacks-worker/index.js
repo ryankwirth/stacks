4a6b63a9945739731d738bc8feff138beac218a2
Ryan Wirth <ryan@ryanwirth.ca>
2021-05-01 16:41:22 -0600

    Fetch instrument information from MSN

--- stacks-worker/index.js
+++ stacks-worker/index.js
@@ -1,12 +1,33 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request));
 });
+
 /**
- * Respond with hello worker text
- * @param {Request} request
+ * Attempt to fetch chart information for the given instrument ID.
+ * @param {Request} request The request to handle.
+ * @returns A Response object.
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+  const { searchParams } = new URL(request.url);
+  const instrumentId = searchParams.get('instrumentId');
+  return await fetchInstrumentData(instrumentId);
+}
+
+/**
+ * Fetch data for the given instrument ID
+ * @param {string} instrumentId The ID of the instrument to look up.
+ * @returns A Response object.
+ */
+async function fetchInstrumentData(instrumentId) {
+  const params = new URLSearchParams({
+    apikey: '0QfOX3Vn51YCzitbLaRkTTBadtWpgTN8NZLW0C1SEM',
+    ids: instrumentId,
+    wrapodata: false,
+    type: 'All',
   });
+
+  const url = new URL('https://assets.msn.com/service/Finance/Charts');
+  url.search = params.toString();
+
+  return await fetch(url);
 }
